03aa1ed3b395694ab1a809f0a4e7bc122112444f
Jason Hill <jason.hill@hcrs.net>
2020-05-11 12:51:28 +0100

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by jason on 11/05/2020.
+//  Copyright © 2020 hcrs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
